c545a994d6fde2398031771b4154fe5e8142ffbd
Cristian16291 <81781116+Cristian16291@users.noreply.github.com>
2025-12-15 18:09:36 +0000

    Generado contenido en castellano desde upstream

---
+++ app/msix-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  packageName: "Warperia-TDLO",
+  packageDisplayName: "Warperia-TDLO",
+  packageDescription: "World of Warcraft Private Servidores Addon Manager Application",
+  packageVersion: "1.2.0.0",
+  publisher: "CN=Warperia-TDLO",
+  publisherDisplayName: "The Warperia-TDLO Team",
+  identityName: "Warperia-TDLO.Warperia-TDLO",
+  assets: "D:\\Files\\Project\\Warperia-TDLO\\msix-assets",
+  inputDirectory: "D:\\Files\\Project\\Warperia-TDLO\\dist\\win-unpacked",
+  outputDirectory: "D:\\Files\\Project\\Warperia-TDLO\\dist\\msix",
+  manifestPath: "D:\\Files\\Project\\Warperia-TDLO\\AppxManifest.xml",
+  capabilities: [
+    "internetClient"
+  ]
+};
---
+++ app/webpack.config.cjs
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public', to: 'public' },
+                { from: 'public/icons', to: 'icons' }, 
+                { from: 'public/renderer.js', to: 'renderer.js' },
+                { from: 'public/title-logo.png', to: 'title-logo.png' },
+            ]
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+        hot: true
+    }
+};
